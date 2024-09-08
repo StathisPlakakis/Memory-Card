f8c1cd56e698fc2c16b8080a40113bd4406eaa45
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-09-08 17:03:19 +0300

    Set up the main lay out of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import './App.css'
+import Pokemons from './components/pokemond'
+import { useState } from 'react'
 
-function App() {}
+function App() {
+
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+
+  return (
+    <>
+      <header>
+        <h1>Pokemon - Memory game</h1>
+        <h3>Click on a Pokemon card <span>(* only once)</span> and collect points</h3>
+        <div className='Scores'>
+          <h3>Score: {score}</h3>
+          <h3>High Score: {highScore}</h3>
+        </div>
+      </header>
+      <Pokemons/>    
+    </>
+  )
+}
 
 export default App
