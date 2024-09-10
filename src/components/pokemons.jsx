51a60dbc1cb6a78e19c29d407980c0f4be64a998
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-09-10 18:22:50 +0300

    Refactor getPokemons to set state with useful info

--- src/components/pokemons.jsx
+++ src/components/pokemons.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 export default function Pokemons() {
   const [pokemons, setPokemons] = useState([]);
 
-  const fetchPokemons = async function () {
+  const getPokemonsData = async () => {
     try {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
+        "https://pokeapi.co/api/v2/pokemon?limit=1&offset=0",
         {
           mode: "cors",
         }
@@ -26,9 +26,57 @@ export default function Pokemons() {
     }
   };
 
+  const getPokemonsUrl = async () => {
+    const pokemonsData = await getPokemonsData();
+    const pokemonsUrl = pokemonsData.map(pokemonData => pokemonData.url)
+    return pokemonsUrl;
+  }
+
+  const getPokemonsName = async () => {
+    const pokemonsData = await getPokemonsData();
+    const pokemonsName = pokemonsData.map(pokemonData => pokemonData.name)
+    return pokemonsName;
+  }
+
+  const getPokemonsImg = async () => {
+    const pokemonsUrl = await getPokemonsUrl();
+    const pokemonsImgPromises = pokemonsUrl.map(async (pokemonUrl) => {
+      try {
+        const response = await fetch(pokemonUrl,
+          {
+            mode: "cors",
+          }
+        );
+        if (response.ok) {
+          const pokemonData = await response.json();
+          const images = await pokemonData.sprites.other.dream_world.front_default;
+          return images;
+        }else {
+          throw new Error(
+          `A ${response.status}, error was thrown, in pokeAPI call!`
+          )
+        }
+      } catch(error) {
+        console.error(error);
+      }
+    })
+
+    const pokemonsImg = await Promise.all(pokemonsImgPromises);
+    return pokemonsImg;
+  }
+
   useEffect(() => {
     const getPokemons = async () => {
-      const pokemonsData = await fetchPokemons();
+      const pokemonsImg = await getPokemonsImg();
+      const pokemonsName = await getPokemonsName();
+      let pokemonsData = []
+      for (let i = 0; i < pokemonsImg.length; i++){
+        const pokemonData = {
+          name: pokemonsName[i],
+          img: pokemonsImg[i]
+        }
+        pokemonsData.push(pokemonData)
+      }
       setPokemons(pokemonsData);
     };
 
@@ -38,7 +86,7 @@ export default function Pokemons() {
   return (
     <div>
       {pokemons.length > 0 ? (
-        pokemons.map((pokemon, index) => <h1 key={index}>{pokemon.name}</h1>)
+        pokemons.map((pokemon, index) => <h1 key={index}>hey</h1>)
       ) : (
         <p>Loading...</p>
       )}
